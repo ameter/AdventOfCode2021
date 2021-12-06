8d295d0ea275533c7fbe7ea8a0e13c2a75ae2f19
Chris Ameter <29186822+ameter@users.noreply.github.com>
2021-12-05 23:45:58 -0700

    Day 6

--- AdventOfCode.playground/Pages/Day 6.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/Day 6.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
 import Foundation
 
-let input = Day6.sample.map {
-    //Array($0).map(String.init).compactMap(Int.init)
-    $0
+let input = Day6.input
+
+// fish state, number of fish in that state
+var fishes = [Int : Int]()
+
+// initialize fishes
+for i in 0...8 {
+    fishes[i] = 0
 }
 
+// load input into fishes
+for fish in input {
+    fishes[fish]! += 1
+}
+
+func updateFish() {
+    let spawns = fishes[0]!
+    
+    for i in 1...8 {
+        fishes[i - 1] = fishes[i]
+    }
+    
+    fishes[8] = spawns
+    fishes[6]! += spawns
+}
+
+// update fishes each day
+for _ in 0..<256 {
+    updateFish()
+}
+
+print(fishes.values.reduce(0, +))
