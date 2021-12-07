6a9eaf08c0d262aeec286e0801ea8f958a3f9fe5
Chris Ameter <29186822+ameter@users.noreply.github.com>
2021-12-06 23:48:30 -0700

    Day 7

--- AdventOfCode.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
+++ AdventOfCode.playground/Pages/Day 7.xcplaygroundpage/Contents.swift
@@ -1,7 +1,11 @@
 import Foundation
 
-let input = Day7.sample.map {
-    //Array($0).map(String.init).compactMap(Int.init)
-    $0
+var input = Day7.input
+let costTable = (input.min()!...input.max()!).map {(0...$0).reduce(0, +)}
+
+let lowest = (input.min()!...input.max()!).reduce(Int.max) { lowest, position in
+    let cost = input.reduce(0) {$0 + costTable[abs(position - $1)]}
+    return min(lowest, cost)
 }
 
+print(lowest)
